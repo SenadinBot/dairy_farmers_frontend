bcbfbbb0686350d6088d57aae19e37d775ead66b
senadin <senadin.botic@kodecta.com>
2019-08-16 16:51:06 +0200

    Improve header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,4 +3,30 @@ $(document).ready(function () {
     $('.video-modal').on('hidden.bs.modal', () => {
         $('.video-modal iframe').attr('src', $('.video-modal iframe').attr('src'));
     });
+
+    // Adding Class on Scroll
+    $(window).on('load', function () {
+        var winOffset = document.documentElement.scrollTop || document.body.scrollTop;
+        if (winOffset > 38.5) {
+            $('body').addClass('nav-fixed');
+        }
+    });
+    $(window).scroll(function () {
+        var winOffset = document.documentElement.scrollTop || document.body.scrollTop;
+        if (winOffset > 38) {
+            $('body').addClass('nav-fixed');
+        } else {
+            $('body').removeClass('nav-fixed');
+        }
+    });
+
+    // Open/Close Desktop Search Open
+    $('.search-icon img').on('click', function () {
+        var $this = $('.header-search');
+        if ($this.is(':hidden')) {
+            $this.slideDown();
+        } else {
+            $this.slideUp();
+        }
+    });
 });
