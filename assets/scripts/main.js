45cbca09dd4712a457fa38eacc120b3cc3067d34
senadin <senadin.botic@kodecta.com>
2019-09-05 14:52:45 +0200

    Added second header

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -212,29 +212,29 @@ $.fn.menumaker = function (options) {
         sticky: false
     }, options);
     return this.each(function () {
-        $(this).find(".button").on('click', function () {
-            $(this).toggleClass('menu-opened');
-            var mainmenu = $(this).next('ul');
-            if (mainmenu.hasClass('open')) {
-                mainmenu.slideToggle().removeClass('open');
-            }
-            else {
-                mainmenu.slideToggle().addClass('open');
-                if (settings.format === "dropdown") {
-                    mainmenu.find('ul').show();
-                }
-            }
-        });
+        // $(this).find(".button").on('click', function () {
+        //     $(this).toggleClass('menu-opened');
+        //     var mainmenu = $(this).next('ul');
+        //     if (mainmenu.hasClass('open')) {
+        //         mainmenu.slideToggle().removeClass('open');
+        //     }
+        //     else {
+        //         mainmenu.slideToggle().addClass('open');
+        //         if (settings.format === "dropdown") {
+        //             mainmenu.find('ul').show();
+        //         }
+        //     }
+        // });
         cssmenu.find('li ul').parent().addClass('has-sub');
         multiTg = function () {
             cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
             cssmenu.find('.submenu-button').on('click', function () {
                 $(this).toggleClass('submenu-opened');
                 if ($(this).siblings('ul').hasClass('open')) {
-                    $(this).siblings('ul').removeClass('open').slideToggle();
+                    $(this).siblings('ul').removeClass('open').slideToggle('fast');
                 }
                 else {
-                    $(this).siblings('ul').addClass('open').slideToggle();
+                    $(this).siblings('ul').addClass('open').slideToggle('fast');
                 }
             });
         };
@@ -242,7 +242,7 @@ $.fn.menumaker = function (options) {
         else cssmenu.addClass('dropdown');
         if (settings.sticky === true) cssmenu.css('position', 'fixed');
         resizeFix = function () {
-            var mediasize = 1201;
+            var mediasize = 1200;
             if ($(window).width() > mediasize) {
                 cssmenu.find('ul').show();
             }
