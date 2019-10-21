4baad5506d7df0a6972b52cd91246d673dfc3e03
Senadin <senadin.botic@gmail.com>
2019-10-21 23:02:56 +0200

    Improve behaviour

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -419,38 +419,6 @@ $(document).ready(function () {
     $('body').on('click', '.explore-main-btn-container .primary-btn', function () {
         $(this).toggleClass('active-btn');
     });
-
-    //Calendar form validation
-    $(".submit-btn").on('click', function (e) {
-        var hasError = false;
-        var PrivacyPolicy = $('#privacyPolicy').prop('checked');
-        var email = $('#emailAddress').val();
-        var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-        e.preventDefault();
-        if (!pattern.test(email)) {
-            hasError = true;
-            $('#emailAddress').parent().parent().addClass('form-error');
-        }
-        else {
-            $('#emailAddress').parent().parent().removeClass('form-error');
-        }
-        $(".required").each(function () {
-            if ($(this).val() === "") {
-                $(this).parent().parent().addClass('form-error');
-                hasError = true;
-            }
-            else {
-                $(this).parent().parent().removeClass('form-error');
-            }
-        });
-        if (!PrivacyPolicy) {
-            $('#privacyPolicy').parent().parent().addClass('form-error');
-            hasError = true;
-        }
-        if (!hasError) {
-            $(".btnSubmitServer")[0].click();
-        }
-    });
 });
 
 $.fn.menumaker = function (options) {
